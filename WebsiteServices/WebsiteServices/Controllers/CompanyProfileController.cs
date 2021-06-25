019710e2aa8e874d204c85f9b5f1ad12ed112271
EmirhanYilmaz <48058646+EmirhanYilmaz@users.noreply.github.com>
2021-06-25 08:35:20 +0200

    web review controller.

---
+++ WebsiteServices/WebsiteServices/Controllers/CompanyProfileController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteServices.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebsiteServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyProfileController : ControllerBase
+    {
+        private readonly DatabaseContext dataContext;
+
+        public CompanyProfileController(DatabaseContext dataContextObj)
+        {
+            dataContext = dataContextObj;
+        }
+        // GET: api/<CompanyProfileController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CompanyProfileController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CompanyProfileController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CompanyProfileController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CompanyProfileController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebsiteServices/WebsiteServices/Controllers/ReviewDetailController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteServices.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebsiteServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewDetailController : ControllerBase
+    {
+        private readonly DatabaseContext dataContext;
+
+        public ReviewDetailController (DatabaseContext dataContextObj)
+        {
+            dataContext = dataContextObj;
+        }
+        // GET: api/<ReviewDetailController>
+        [HttpGet]
+        public IEnumerable<ReviewDetail> GetReviews()
+        {
+            List<CompanyProfile> companyProfilesList = dataContext.CompanyProfiles.ToList();
+            
+            List<User> userList = dataContext.Users.ToList();
+
+            List<ReviewDetail> reviewsList = dataContext.ReviewDetails.ToList();
+            return reviewsList;
+
+        }
+        [HttpGet("company")]
+        public IEnumerable<ReviewDetail> GetReviewByURL(string url)      
+        {
+            List<CompanyProfile> companyProfilesList = dataContext.CompanyProfiles.ToList();
+
+            List<User> userList = dataContext.Users.ToList();
+
+            List<ReviewDetail> reviewsList = dataContext.ReviewDetails.ToList();
+            var companyByUrl = reviewsList.Where(webUrl => webUrl.company.companyURL == url);
+            return companyByUrl;
+
+        }
+
+        [HttpGet("rating")]
+        public int GetAvrgRating(string url)
+        {
+            List<CompanyProfile> companyProfilesList = dataContext.CompanyProfiles.ToList();
+
+            List<User> userList = dataContext.Users.ToList();
+
+            List<ReviewDetail> reviewsList = dataContext.ReviewDetails.ToList();
+            var companyByUrl = reviewsList.Where(reviewTotal => reviewTotal.company.companyURL == url).Select(totalReviewRating => totalReviewRating.reviewRating);
+
+            int ratingSum = companyByUrl.Sum();
+            int avrgRating = ratingSum / companyByUrl.Count();
+
+            return avrgRating;              
+        }
+
+        [HttpGet("totalratings")]
+        public int TotalRatings(string url)
+        {
+            List<CompanyProfile> companyProfilesList = dataContext.CompanyProfiles.ToList();
+
+            List<User> userList = dataContext.Users.ToList();
+
+            List<ReviewDetail> reviewsList = dataContext.ReviewDetails.ToList();
+            var companyByUrl = reviewsList.Where(reviewTotal => reviewTotal.company.companyURL == url).Select(totalReviewRating => totalReviewRating.reviewRating);
+
+            int totalRatings = companyByUrl.Count();
+
+            return totalRatings;
+        }
+
+
+
+
+
+        // POST api/<ReviewDetailController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ReviewDetailController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ReviewDetailController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
+class List
+{
+    List (int size)
+    {
+        firstElement = new Element();
+        for (int i = 0; i < size; i++)
+        {
+            firstElement.next = new Element();
+        }
+    }
+    Element firstElement;
+}
+
+class Element
+{
+    public Element next;
+}
